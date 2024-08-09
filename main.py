90c7606aea6dcc3b3d5761d331d77c6295e82f12
hakim <akborislamamir5555.com>
2024-08-09 17:02:31 +0600

    data ingestoin added

---
+++ main.py
@@ -0,0 +1,11 @@
+from machinetranslation.pipeline.state01_data_ingestion import DataIngestionPipeline
+from machinetranslation.logging import logger
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
